22d98034c78acdfcc701b3ebec45c6ede3e88694
Karinna Rocha - NurVIX <75399228+karinnarocha@users.noreply.github.com>
2024-07-19 23:18:23 -0400

    Delete PORTIFOLIO/NurvixPage directory

--- PORTIFOLIO/NurvixPage/routes.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-nurvixPage = Blueprint('nurvixPage', __name__)
-
-
-@nurvixPage.route("/NurVIX")
-def nurvixpage():
-    return render_template("NurVIX.html")
