0e8de5edb65497d7e2796696960cf9ac79851c2e
karinnarocha <karinna.rocha2@gmail.com>
2024-07-20 00:22:37 -0400

    Initial commit

--- PORTIFOLIO/__init__.py
+++ PORTIFOLIO/__init__.py
@@ -15,14 +15,11 @@ def create_app():
     from .WorkPage.routes import workPage
     from .AboutPage.routes import aboutPage
     from .ContactPage.routes import contactPage
-    from .NurvixPage.routes import nurvixPage
-    from .AIBeansPage.routes import AIBeansPage
+
 
     app.register_blueprint(indexPage)
     app.register_blueprint(workPage)
     app.register_blueprint(aboutPage)
     app.register_blueprint(contactPage)
-    app.register_blueprint(nurvixPage)
-    app.register_blueprint(AIBeansPage)
 
     return app
