c70811404d2135cfe830301993f60a68d8ca29cb
karinnarocha <karinna.rocha2@gmail.com>
2024-07-19 23:25:10 -0400

    deploy backend

--- PORTIFOLIO/AIBeansPage/routes.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-AIBeansPage = Blueprint('AIBensPage', __name__)
-
-@AIBeansPage.route("/AIBeans")
-def aibeanspage():
- return render_template("AIBEans.html")
