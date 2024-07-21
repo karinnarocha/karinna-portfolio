8d3c8c9f6d53e78ac4e09a35c905ad7a71805394
karinnarocha <karinna.rocha2@gmail.com>
2024-07-21 14:47:28 -0400

    Resolve merge conflicts

--- PORTIFOLIO/config.py
+++ PORTIFOLIO/config.py
@@ -1,14 +1,12 @@
 import os
-from dotenv import load_dotenv, find_dotenv
 
-load_dotenv(find_dotenv())
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    MAIL_SERVER = os.getenv('MAIL_SERVER')
-    MAIL_PORT = int(os.getenv('MAIL_PORT'))
-    MAIL_USE_TLS = os.getenv('MAIL_USE_TLS').lower() in ['true', '1']
-    MAIL_USE_SSL = os.getenv('MAIL_USE_SSL').lower() in ['true', '1']
-    MAIL_USERNAME = os.getenv('MAIL_USERNAME')
-    MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
-
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT'))
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') == 'True'
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL') == 'True'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER')
